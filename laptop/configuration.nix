df14dcfa5f5e42f0f9ee65e7450ecf2ef7d05710
Niklas Thorne <notrupertthorne@gmail.com>
2017-01-13 23:41:48 +0100

    Added initial NixOS configuration for my laptop

---
+++ laptop/configuration.nix
@@ -0,0 +1,95 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  # boot.loader.grub.efiSupport = true;
+  # boot.loader.grub.efiInstallAsRemovable = true;
+  # boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  # Define on which hard drive you want to install Grub.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+  networking.hostName = "nixlaptop"; # Define your hostname.
+  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Select internationalisation properties.
+  i18n = {
+    consoleFont = "Lat2-Terminus16";
+    consoleKeyMap = "sv-latin1";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Stockholm";
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = with pkgs; [
+    ag
+    direnv
+    dmenu
+    dzen2
+    firefox
+    fzf
+    git
+    neovim
+    terminus_font
+    tmux
+    xsel
+    zsh
+  ];
+
+  # List services that you want to enable:
+  services.nixosManual.showManual = true;
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver = {
+    enable = true;
+    layout = "se";
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+    };
+    windowManager.default = "xmonad";
+    displayManager = {
+      slim = {
+	enable = true;
+	autoLogin = true;
+	defaultUser = "nthorne";
+      };
+    };
+  };
+
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.extraUsers.nthorne = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "wheel" ];
+    createHome = true;
+    home = "/home/nthorne";
+    shell = "/run/current-system/sw/bin/zsh";
+  };
+
+  security.sudo.enable = true;
+
+  # The NixOS release to be compatible with for stateful data such as databases.
+  system.stateVersion = "16.09";
+
+}
---
+++ laptop/install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Sets up environment
+
+echo "Installing configuration.nix.."
+sudo rm /etc/nixos/configuration.nix
+sudo ln -s $PWD/configuration.nix /etc/nixos/
+
+echo "Installing settings repositories.."
+test -d $HOME/.vim || git clone git@github.com:nthorne/nthorne-vim-environment.git $HOME/.vim
+test -d $HOME/.config/nvim || (mkdir $HOME/.config ; ln -s $HOME/.vim $HOME/.config/nvim)
+test -f $HOME/.vimrc || ln -s $HOME/.vim/.vimrc $HOME/
+
+test -d $HOME/.zsh || git clone git@github.com:nthorne/nthorne-zsh-environment.git $HOME/.zsh
+test -f $HOME/.zprofile || ln -s $HOME/.zsh/.zprofile $HOME/
+test -f $HOME/.zshenv || ln -s $HOME/.zsh/.zshenv $HOME/
+test -f $HOME/.zshrc || ln -s $HOME/.zsh/.zshrc $HOME/
+
+test -f $HOME/repos || mkdir $HOME/repos
+pushd $HOME/repos
+test -d nthorne-xmonad-environment || git clone git@github.com:nthorne/nthorne-xmonad-environment.git
+test -d $HOME/.xmonad || mkdir $HOME/.xmonad
+test -f $HOME/.xmonad/xmonad.hs || ln -s $PWD/nthorne-xmonad-environment/xmonad-nixos.hs $HOME/.xmonad/xmonad.hs
+
+test -d nthorne-tmux-environment || git clone git@github.com:nthorne/nthorne-tmux-environment.git
+test -f $HOME/.tmux.conf || ln -s $PWD/nthorne-tmux-environment/.tmux.conf $HOME/
+popd
+
+test -f $HOME/.Xresources || ln -s $PWD/.Xresources $HOME/
