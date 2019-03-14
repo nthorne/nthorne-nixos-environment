a0a5f89820cb6dc11d97df789ade9bcc29a04c35
Niklas Thorne <notrupertthorne@gmail.com>
2019-03-14 13:19:23 +0100

    Findings after fresh install

--- work/README.md
+++ work/README.md
@@ -14,3 +14,13 @@ Clone all dot-repos (follow the respective README):
 Generate a hash for the proxy, and update proxy.nix, according to comments.
 
 Symlink all the files under `dotfiles/` into the proper places.
+
+Re-create the samba credentials file (`/etc/nixos/smb-secrets`).
+
+Set up the cronjob for my backup script.
+
+
+Scripted
+--------
+
+*WARNING:* This is untested, but hey - what could go wrong?!
---
+++ work/install.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+
+# vim: filetype=sh
+
+# Set IFS explicitly to space-tab-newline to avoid tampering
+IFS=' 	
+'
+
+
+NAME=
+# Make sure that we're not on NixOS; if so, avoid tampering with PATH
+if [[ -f /etc/os-release ]]
+then
+  . /etc/os-release
+fi
+
+if [[ "NixOS" != "$NAME" ]]
+then
+  # If found, use getconf to constructing a reasonable PATH, otherwise
+  # we set it manually.
+  if [[ -x /usr/bin/getconf ]]
+  then
+    PATH=$(/usr/bin/getconf PATH)
+  else
+    PATH=/bin:/usr/bin:/usr/local/bin
+  fi
+fi
+
+function error()
+{
+  echo "Error: $@" >&2
+  exit 1
+}
+
+if [[ $(basename ${PWD}) != "work" ]]
+then
+  error "Script needs to be executed from the work folder, not $(basename ${PWD})"
+fi
+
+test -d "${HOME}/repos" || error "Missing folder ${HOME}/repos"
+
+echo "Installing scripts.."
+mkdir $HOME/bin
+ln -s ${PWD}/scripts/*.sh ${HOME}/bin/
+ln -s ${PWD}/scripts/*.xdo ${HOME}/bin/
+
+test -d ${HOME}/.ssh && error "${HOME}/.ssh exists. Bailing out."
+echo "Installing dotfiles"
+ln -s ${PWD}/dotfiles/.ssh ${HOME}/
+ln -s ${PWD}/dotfiles/.tmuxinator ${HOME}/
+ln -s ${PWD}/dotfiles/.gitconfig ${HOME}/
+ln -s ${PWD}/dotfiles/.minirc.usb* ${HOME}/
+ln -s ${PWD}/dotfiles/.Xresources ${HOME}/
+
+echo "Cloning environment repos if needed.."
+pushd ${HOME}/repos
+test -d nthorne-tmux-environment   || git clone https://github.com/nthorne-tmux-environment
+test -d nthorne-vim-environment    || git clone https://github.com/nthorne-vim-environment
+test -d nthorne-xmonad-environment || git clone https://github.com/nthorne-xmonad-environment
+test -d nthorne-zsh-environment    || git clone https://github.com/nthorne-zsh-environment
+popd
+
+echo "Installing environments"
+pushd ${HOME}/repos/nthorne-tmux-environment
+./install-work.sh
+popd
+pushd ${HOME}/repos/nthorne-vim-environment
+./install-work.sh
+popd
+pushd ${HOME}/repos/nthorne-xmonad-environment
+./install-work.sh
+popd
