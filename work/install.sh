ec9ea7ecc58c1f1c255bd7698b9dac8327d968d2
Niklas Thorne <notrupertthorne@gmail.com>
2021-08-09 09:43:31 +0200

    Modularised git config

--- work/install.sh
+++ work/install.sh
@@ -52,6 +52,8 @@ echo "Installing dotfiles"
 ln -s ${PWD}/dotfiles/.ssh ${HOME}/
 ln -s ${PWD}/dotfiles/.tmuxinator ${HOME}/
 ln -s ${PWD}/dotfiles/.gitconfig ${HOME}/
+ln -s ${PWD}/dotfiles/.gitconfig-private ${HOME}/
+ln -s ${PWD}/dotfiles/.gitignore-global ${HOME}/
 ln -s ${PWD}/dotfiles/.minirc.usb* ${HOME}/
 ln -s ${PWD}/dotfiles/.Xresources ${HOME}/
 mkdir -p ${HOME}/.lnav/formats/
