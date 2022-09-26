0988e2872f82ac950228206f153348ff0f0315fb
Niklas Thörne <niklas.thorne@netclean.com>
2022-09-26 07:23:34 +0200

    Updated niv sources
    slack-dark has been renamed to slack

--- home-manager/packages/slack/default.nix
+++ home-manager/packages/slack/default.nix
@@ -1,11 +1,11 @@
 { unstable, lib, pkgs, ... }:
 let
   utils = import ../../utils;
-  slack-offloaded = utils.nvidia-offload pkgs unstable.slack-dark "slack";
+  slack-offloaded = utils.nvidia-offload pkgs unstable.slack "slack";
 in
 {
   home.packages = with unstable; [
-    slack-dark
+    slack
     slack-offloaded
   ];
 }
