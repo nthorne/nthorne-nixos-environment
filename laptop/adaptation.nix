f364e3c98be84e546fcde358082c0afad655e695
Niklas Thörne <notrupertthorne@gmail.com>
2024-07-07 10:54:47 +0200

    Enable steam

--- laptop/adaptation.nix
+++ laptop/adaptation.nix
@@ -61,5 +61,12 @@ in
   nix.extraOptions = ''
       experimental-features = nix-command flakes
       '';
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 }
 
