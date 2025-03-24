bdbb82f31f1d73d6a5683f2f7044915436d1c6c9
Niklas Thörne <niklas.thorne@netclean.com>
2025-03-24 08:04:36 +0100

    nixvim: added treesitter-context

--- home-manager/dotfiles/nixvim/plugins/treesitter.nix
+++ home-manager/dotfiles/nixvim/plugins/treesitter.nix
@@ -1,16 +1,19 @@
 {...}: {
   programs.nixvim = {
-    plugins.treesitter = {
-      enable = true;
-      folding = true;
-      settings = {
-        highlight = {
-          enable = true;
-        };
-        indent = {
-          enable = true;
+    plugins = {
+      treesitter = {
+        enable = true;
+        folding = true;
+        settings = {
+          highlight = {
+            enable = true;
+          };
+          indent = {
+            enable = true;
+          };
         };
       };
+      treesitter-context.enable = true;
     };
   };
 }
