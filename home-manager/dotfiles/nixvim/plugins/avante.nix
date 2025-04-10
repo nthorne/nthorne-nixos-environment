a992b41845ca1b8575a9bbb73fc6649a92966c70
Niklas Thörne <niklas.thorne@netclean.com>
2025-04-10 07:36:33 +0200

    nixvim: Switch avante to main(-ish)

--- home-manager/dotfiles/nixvim/plugins/avante.nix
+++ home-manager/dotfiles/nixvim/plugins/avante.nix
@@ -1,25 +1,62 @@
-{...}: {
-  # EVAL: This one works decently, but its usefulness is more on par with copilot.
-  #       Looks to be lively development, tough, so it will probably get better.  programs.nixvim = {
+{pkgs, ...}: let
+  avanteMain = pkgs.vimPlugins.avante-nvim.overrideAttrs (old: {
+    src = pkgs.fetchFromGitHub {
+      owner = "yetone";
+      repo = "avante.nvim";
+      rev = "87ea15bb94f0707a5fd154f11f5ed419c17392d1";
+      sha256 = "sha256-zZzLDfxAe7SAwVqDoTAO9SFc0OdS0wctzoN+E/qZu8E=";
+    };
+    version = "2025-04-09";
+    # Require setup so we skip these.
+    nvimSkipModules = [
+      "avante.providers.vertex_claude"
+      "avante.providers.azure"
+      "avante.providers.ollama"
+      "avante.providers.copilot"
+    ];
+
+    dependencies = with pkgs.vimPlugins; [
+      copilot-lua
+      dressing-nvim
+      fzf-lua
+      img-clip-nvim
+      mini-pick
+      nui-nvim
+      nvim-cmp
+      nvim-treesitter
+      nvim-web-devicons
+      plenary-nvim
+      telescope-nvim
+    ];
+  });
+in {
+  # References:
+  #   - https://github.com/yetone/avante.nvim/issues/1807
+  #   - https://github.com/yetone/avante.nvim/issues/1813
   programs.nixvim = {
     plugins = {
       avante = {
         enable = true;
 
+        package = avanteMain;
+
         settings = {
-          provider = "ollama";
-          vendors = {
-            ollama = {
-              __inherited_from = "openai";
-              api_key_name = "";
-              endpoint = "http://127.0.0.1:11434/v1";
-              model = "qwen2.5-coder:latest";
-              stream = true;
-            };
+          provider = "copilot";
+          copilot = {
+            model = "claude-3.7-sonnet";
+          };
+          copilotthink = {
+            __inherited_from = "copilot";
+            model = "claude-3.7-sonnet-thought";
+            timeout = 600000;
+            max_completion_tokens = 64000;
+          };
+          ollama = {
+            model = "qwen2.5-coder:latest";
           };
           # Recommended for ollama
           behaviour = {
-            enable_cursor_planning_mode = true;
+            enable_cursor_planning_mode = false;
           };
         };
       };
