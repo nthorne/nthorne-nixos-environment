6d30be4543d036bc9f5b176fc2ff4fb637dab003
Niklas Thörne <niklas.thorne@netclean.com>
2025-04-11 08:22:36 +0200

    nixvim: incremental selection for treesitter

--- home-manager/dotfiles/nixvim/plugins/treesitter.nix
+++ home-manager/dotfiles/nixvim/plugins/treesitter.nix
@@ -11,6 +11,16 @@
           indent = {
             enable = true;
           };
+
+          incremental_selection = {
+            enable = true;
+            keymaps = {
+              init_selection = "<C-space>";
+              node_incremental = "<C-space>";
+              scope_incremental = false;
+              node_decremental = "<bs>";
+            };
+          };
         };
       };
       treesitter-context.enable = true;
