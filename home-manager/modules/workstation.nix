04890d759e5f8487fa3432219cd8442855db4687
Niklas Thörne <niklas.thorne@netclean.com>
2025-06-30 11:11:42 +0200

    gemini-cli: switch to official package, but override sources
    Closes #5

--- home-manager/modules/workstation.nix
+++ home-manager/modules/workstation.nix
@@ -2,14 +2,31 @@
   pkgs,
   flake-inputs,
   ...
-}: {
+}: let
+  # TODO: Drop this one once the official package catches up.
+  gemini-cli-latest = pkgs.gemini-cli.overrideAttrs (final: previous: rec {
+    version = "0.1.7";
+    src = pkgs.fetchFromGitHub {
+      owner = "google-gemini";
+      repo = "gemini-cli";
+      rev = "19d2a0fb35ff75ebbed2dda5c8574ffcc66cd4d5";
+      sha256 = "sha256-DAenod/w9BydYdYsOnuLj7kCQRcTnZ81tf4MhLUug6c=";
+    };
+    # Set to empy string to get hash
+    npmDepsHash = "sha256-otogkSsKJ5j1BY00y4SRhL9pm7CK9nmzVisvGCDIMlU=";
+    npmDeps = pkgs.fetchNpmDeps {
+      inherit src;
+      name = "${previous.pname}-${version}-npm-deps";
+      hash = npmDepsHash;
+    };
+  });
+in {
   imports = [
     flake-inputs.sops-nix.homeManagerModules.sops
-
-    ../packages/gemini 
   ];
   home.packages = with pkgs; [
     bitwarden
+    gemini-cli-latest
     gh
     gparted
     grimblast # Screen capture tool with hyprland support
