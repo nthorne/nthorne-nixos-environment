cf72900b1cf2f343133091a2e388fee2f3dd6e76
Niklas Thörne <niklas.thorne@netclean.com>
2025-07-04 12:57:14 +0200

    all: created tmux-boom for quick closing of terminals

--- home-manager/packages/tmux-sessionizer/default.nix
+++ home-manager/packages/tmux-sessionizer/default.nix
@@ -1,6 +1,24 @@
-{pkgs, ...}: {
-  home.packages = [
-    pkgs.tmux-sessionizer
+{pkgs, ...}: let
+  # Perhaps not entirely tied to sessionizer, but making it easier to
+  # start sesssions kinda makes this a requirement.
+  tmux-boom = pkgs.writeShellApplication {
+    name = "tmux-boom";
+    runtimeInputs = [pkgs.bashInteractive];
+    text = ''
+      #!/usr/bin/env bash
+      # Send Ctrl-d to all panes, except the current one.
+      current_pane=$(tmux display-message -p '#{pane_id}')
+      for pane in $(tmux list-panes -a -F '#{pane_id}'); do
+          if [[ "$pane" != "$current_pane" ]]; then
+              tmux send-keys -t "$pane" C-d
+          fi
+      done
+    '';
+  };
+in {
+  home.packages = with pkgs; [
+    tmux-boom
+    tmux-sessionizer
   ];
 
   home.file.".config/tms/config.toml" = {
@@ -19,5 +37,6 @@
     bind t display-popup -E "tms"
     bind T display-popup -E "tms switch"
     bind W display-popup -E "tms windows"
+    bind b confirm-before -p "Send Ctrl-d to all panes? (y/n)" "run tmux-boom"
   '';
 }
