d6a3000135e3d99cea3a902bf88e64e452a8682b
Niklas Thörne <niklas.thorne@netclean.com>
2025-10-03 12:34:19 +0200

    vimes: drop old virtual webcam config parts

--- work/adaptation.nix
+++ work/adaptation.nix
@@ -256,20 +256,9 @@ in {
     kernelModules = [
       "nbd"
       "uvcvideo"
-      "akvcam"
-      "v4l2loopback"
     ];
     tmp.useTmpfs = true;
-    extraModulePackages = [config.boot.kernelPackages.v4l2loopback.out];
   };
-
-  # Set initial kernel module settings
-  boot.extraModprobeConfig = ''
-    # exclusive_caps: Skype, Zoom, Teams etc. will only show device when actually streaming
-    # card_label: Name of virtual camera, how it'll show up in Skype, Zoom, Teams
-    # https://github.com/umlaeute/v4l2loopback
-    options v4l2loopback devices=1 video_nr=10 card_label="OBS Cam" exclusive_caps=1
-  '';
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.nthorne.extraGroups = [
