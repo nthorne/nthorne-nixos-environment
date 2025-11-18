1adaee4437551ffd74199596dd9f271345844ec3
Niklas Thörne <niklas.thorne@netclean.com>
2025-11-18 14:32:19 +0100

    nixvim: add mappings to send telescopes to trouble

--- home-manager/dotfiles/nixvim/plugins/telescope.nix
+++ home-manager/dotfiles/nixvim/plugins/telescope.nix
@@ -60,6 +60,20 @@
             action = "marks";
           };
         };
+        luaConfig.post = ''
+          local actions = require("telescope.actions")
+          local open_with_trouble = require("trouble.sources.telescope").open
+          local telescope = require("telescope")
+
+          telescope.setup({
+            defaults = {
+              mappings = {
+                i = { ["<c-t>"] = open_with_trouble },
+                n = { ["<c-t>"] = open_with_trouble },
+              },
+            },
+          })
+        '';
       };
       which-key.settings = {
         spec = [
