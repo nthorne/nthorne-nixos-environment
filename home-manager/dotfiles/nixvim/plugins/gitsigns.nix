3d286773c9422916c0a35c007f16056e0e6835ec
Niklas Thörne <niklas.thorne@netclean.com>
2025-12-04 10:07:19 +0100

    nixvim: updated gitsigns keymappings

--- home-manager/dotfiles/nixvim/plugins/gitsigns.nix
+++ home-manager/dotfiles/nixvim/plugins/gitsigns.nix
@@ -10,51 +10,51 @@
       gitsigns.setup()
 
     -- Navigation
-    vim.keymap.set('n', ']c', function()
+    vim.keymap.set('n', ']h', function()
       if vim.wo.diff then
-        vim.cmd.normal({']c', bang = true})
+        vim.cmd.normal({']h', bang = true})
       else
         gitsigns.nav_hunk('next')
       end
-    end)
+    end, { desc = "Go to Next Hunk" })
 
-    vim.keymap.set('n', '[c', function()
+    vim.keymap.set('n', '[h', function()
       if vim.wo.diff then
-        vim.cmd.normal({'[c', bang = true})
+        vim.cmd.normal({'[h', bang = true})
       else
         gitsigns.nav_hunk('prev')
       end
-    end)
+    end, { desc = "Go to Previous Hunk" })
 
     -- Actions
-    vim.keymap.set('n', '<leader>ghs', gitsigns.stage_hunk, { desc = "Stage Hunk" })
-    vim.keymap.set('n', '<leader>ghr', gitsigns.reset_hunk, { desc = "Reset Hunk" })
+    vim.keymap.set('n', '<leader>gs', gitsigns.stage_hunk, { desc = "Stage Hunk" })
+    vim.keymap.set('n', '<leader>gr', gitsigns.reset_hunk, { desc = "Reset Hunk" })
 
-    vim.keymap.set('v', '<leader>ghs', function()
+    vim.keymap.set('v', '<leader>gs', function()
       gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
     end, { desc = "Stage Hunk (visual)" })
 
-    vim.keymap.set('v', '<leader>ghr', function()
+    vim.keymap.set('v', '<leader>gr', function()
       gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
     end, { desc = "Reset Hunk (visual)" })
 
-    vim.keymap.set('n', '<leader>ghS', gitsigns.stage_buffer, { desc = "Stage Buffer" })
-    vim.keymap.set('n', '<leader>ghR', gitsigns.reset_buffer, { desc = "Reset Buffer" })
-    vim.keymap.set('n', '<leader>ghp', gitsigns.preview_hunk, { desc = "Preview Hunk" })
-    vim.keymap.set('n', '<leader>ghi', gitsigns.preview_hunk_inline, { desc = "Preview Hunk Inline" })
+    vim.keymap.set('n', '<leader>gS', gitsigns.stage_buffer, { desc = "Stage Buffer" })
+    vim.keymap.set('n', '<leader>gR', gitsigns.reset_buffer, { desc = "Reset Buffer" })
+    vim.keymap.set('n', '<leader>gp', gitsigns.preview_hunk, { desc = "Preview Hunk" })
+    vim.keymap.set('n', '<leader>gi', gitsigns.preview_hunk_inline, { desc = "Preview Hunk Inline" })
 
-    vim.keymap.set('n', '<leader>ghb', function()
+    vim.keymap.set('n', '<leader>gb', function()
       gitsigns.blame_line({ full = true })
     end, { desc = "Blame Line" })
 
-    vim.keymap.set('n', '<leader>ghd', gitsigns.diffthis, { desc = "Diff This" })
+    vim.keymap.set('n', '<leader>gd', gitsigns.diffthis, { desc = "Diff This" })
 
-    vim.keymap.set('n', '<leader>ghD', function()
+    vim.keymap.set('n', '<leader>gD', function()
       gitsigns.diffthis('~')
     end, { desc = "Diff This (against HEAD)" })
 
-    vim.keymap.set('n', '<leader>ghQ', function() gitsigns.setqflist('all') end, { desc = "Set QF List (all)" })
-    vim.keymap.set('n', '<leader>ghq', gitsigns.setqflist, { desc = "Set QF List (staged)" })
+    vim.keymap.set('n', '<leader>gQ', function() gitsigns.setqflist('all') end, { desc = "Set QF List (all)" })
+    vim.keymap.set('n', '<leader>gq', gitsigns.setqflist, { desc = "Set QF List (staged)" })
 
     -- Toggles
     vim.keymap.set('n', '<leader>gtb', gitsigns.toggle_current_line_blame, { desc = "Toggle Current Line Blame" })
