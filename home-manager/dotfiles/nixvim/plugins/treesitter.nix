e4d48720cf5598e72ea20d7b35b74132c95e0a2e
Niklas Thörne <niklas.thorne@netclean.com>
2025-12-04 09:21:00 +0100

    nixvim: update treesitter mappings

--- home-manager/dotfiles/nixvim/plugins/treesitter.nix
+++ home-manager/dotfiles/nixvim/plugins/treesitter.nix
@@ -28,30 +28,48 @@
 
       treesitter-textobjects = {
         enable = true;
-
-        settings.move = {
-          enable = true;
-          gotoNextStart.__raw = ''
-            {
-              ["](b"] = "@block.outer",
-              ["](f"] = "@function.outer",
-            }'';
-          gotoNextEnd.__raw = ''
-            {
-              ["])b"] = "@block.outer",
-              ["])f"] = "@function.outer",
-            }'';
-          gotoPreviousStart.__raw = ''
-            {
-              ["[(b"] = "@block.outer",
-              ["[(f"] = "@function.outer",
-            }'';
-          gotoPreviousEnd.__raw = ''
-            {
-              ["[)b"] = "@block.outer",
-              ["[)f"] = "@function.outer",
-            }'';
-        };
+        settings.__raw = ''
+        {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@conditional.outer",
+              ["ic"] = "@conditional.inner",
+              ["al"] = "@loop.outer",
+              ["il"] = "@loop.inner",
+            },
+          },
+          move = {
+            enable = true,
+            goto_next_start = {
+              ["]f"] = "@function.outer",
+              ["]c"] = "@conditional.outer",
+              ["]l"] = "@loop.outer",
+              ["]b"] = "@block.outer",
+            },
+            goto_previous_start = {
+              ["[f"] = "@function.outer",
+              ["[c"] = "@conditional.outer",
+              ["[l"] = "@loop.outer",
+              ["[b"] = "@block.outer",
+            },
+            goto_next_end = {
+              ["]F"] = "@function.outer",
+              ["]C"] = "@conditional.outer",
+              ["]L"] = "@loop.outer",
+              ["]B"] = "@block.outer",
+            },
+            goto_previous_end = {
+              ["[F"] = "@function.outer",
+              ["[C"] = "@conditional.outer",
+              ["[L"] = "@loop.outer",
+              ["[B"] = "@block.outer",
+            },
+          },
+        }'';
       };
     };
   };
