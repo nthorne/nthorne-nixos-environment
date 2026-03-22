7f3b99526478393e1b0dea9e0ea1fc8dce008bdd
Niklas Thörne <notrupertthorne@gmail.com>
2026-03-22 09:41:34 +0100

    nixlaptop: docker -> podman

--- laptop/adaptation.nix
+++ laptop/adaptation.nix
@@ -65,7 +65,12 @@ in
     "networkmanager"
   ];
 
-  virtualisation.docker.enable = true;
+  virtualisation = {
+    podman = {
+      enable = true;
+      dockerCompat = true;
+    };
+  };
 
   nix.extraOptions = ''
     experimental-features = nix-command flakes
