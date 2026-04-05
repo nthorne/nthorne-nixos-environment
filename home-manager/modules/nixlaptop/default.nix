f7ab8f739b941a7892bdd38118d6f571950d2871
Niklas Thörne <notrupertthorne@gmail.com>
2026-04-05 14:28:58 +0200

    nixlaptop: enable some ollama cloud models for opencode

--- home-manager/modules/nixlaptop/default.nix
+++ home-manager/modules/nixlaptop/default.nix
@@ -25,4 +25,20 @@ in {
       "all/atuin/session".path = "/home/nthorne/.local/share/atuin/session";
     };
   };
+
+  # Also add these cloud models for private use
+  programs.opencode.settings.provider.ollama.models = {
+    "glm-5:cloud" = {
+      name = "glm-5:cloud";
+    };
+    "qwen3.5:397b-cloud" = {
+      name = "qwen3.5:397b-cloud";
+    };
+    "qwen3-coder-next:cloud" = {
+      name = "qwen3-coder-next:cloud";
+    };
+    "gemini-3-flash-preview:cloud" = {
+      name = "gemini-3-flash-preview:cloud";
+    };
+  };
 }
