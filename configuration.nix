c5a26ba79faafee7bae1a1c57dc01b43c237e65b
Niklas Thörne <notrupertthorne@gmail.com>
2026-06-13 17:35:51 +0200

    update flake.nix, lockfile and drop configuration.nix modification

--- configuration.nix
+++ configuration.nix
@@ -62,6 +62,11 @@ in {
       };
     };
   };
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+    xwayland.enable = true;
+  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.nthorne = {
