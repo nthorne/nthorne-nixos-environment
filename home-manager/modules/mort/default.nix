0fe2b63142bbd996f3916cd9cf8ec7fc47f478ae
Niklas Thörne <niklas.thorne@logisnext.eu>
2026-08-17 08:13:09 +0200

    mort: route GitHub SSH requests to HTTPS
    SSH is blocked so fetching private secrets repo fails, but
    if we route GitHub requests to HTTPS instead, fetching secrets
    repo works just fine as long as the host SSH key is added to
    the list of trusted keys in GitHub.

--- home-manager/modules/mort/default.nix
+++ home-manager/modules/mort/default.nix
@@ -22,5 +22,18 @@
       useGHE = true;
       gheURL = "https://logisnext.ghe.com/";
     };
+
+    programs.ssh = {
+      enable = true;
+      enableDefaultConfig = false;
+
+      settings = {
+        "github.com" = {
+          hostname = "ssh.github.com";
+          port = 443;
+          user = "git";
+        };
+      };
+    };
   };
 }
