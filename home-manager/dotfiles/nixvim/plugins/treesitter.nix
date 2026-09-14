65566e2f223f3c6eb8a1bed3fe87d286e74afc4b
Niklas Thörne <niklas.thorne@logisnext.eu>
2026-09-14 09:56:17 +0200

    nixvim: add descriptions to treesitter bindings

--- home-manager/dotfiles/nixvim/plugins/treesitter.nix
+++ home-manager/dotfiles/nixvim/plugins/treesitter.nix
@@ -71,6 +71,35 @@
           },
         }'';
       };
+
+      which-key.settings.spec = [
+        { __unkeyed = "af"; mode = [ "o" "x" ]; desc = "Around function"; }
+        { __unkeyed = "if"; mode = [ "o" "x" ]; desc = "Inside function"; }
+        { __unkeyed = "ac"; mode = [ "o" "x" ]; desc = "Around conditional"; }
+        { __unkeyed = "ic"; mode = [ "o" "x" ]; desc = "Inside conditional"; }
+        { __unkeyed = "al"; mode = [ "o" "x" ]; desc = "Around loop"; }
+        { __unkeyed = "il"; mode = [ "o" "x" ]; desc = "Inside loop"; }
+
+        { __unkeyed = "]f"; mode = "n"; desc = "Next function start"; }
+        { __unkeyed = "[f"; mode = "n"; desc = "Prev function start"; }
+        { __unkeyed = "]F"; mode = "n"; desc = "Next function end"; }
+        { __unkeyed = "[F"; mode = "n"; desc = "Prev function end"; }
+
+        { __unkeyed = "]c"; mode = "n"; desc = "Next conditional start"; }
+        { __unkeyed = "[c"; mode = "n"; desc = "Prev conditional start"; }
+        { __unkeyed = "]C"; mode = "n"; desc = "Next conditional end"; }
+        { __unkeyed = "[C"; mode = "n"; desc = "Prev conditional end"; }
+
+        { __unkeyed = "]l"; mode = "n"; desc = "Next loop start"; }
+        { __unkeyed = "[l"; mode = "n"; desc = "Prev loop start"; }
+        { __unkeyed = "]L"; mode = "n"; desc = "Next loop end"; }
+        { __unkeyed = "[L"; mode = "n"; desc = "Prev loop end"; }
+
+        { __unkeyed = "]b"; mode = "n"; desc = "Next block start"; }
+        { __unkeyed = "[b"; mode = "n"; desc = "Prev block start"; }
+        { __unkeyed = "]B"; mode = "n"; desc = "Next block end"; }
+        { __unkeyed = "[B"; mode = "n"; desc = "Prev block end"; }
+      ];
     };
   };
 }
